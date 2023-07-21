debf81a901762905a853aa32bc8cb56c1bb005d3
Diego Sousa <khesanh@Diegos-MacBook-Pro.local>
2023-07-21 10:20:31 -0400

    Create db/connection.js and succesfully connected to mongoDB

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+import chalk from "chalk"
+
+mongoose.set('returnOriginal', false)
+
+mongoose.connect("mongodb://127.0.0.1:27017/my-api")
+  .catch((err) => {
+    console.log(`Can not connect to mongoDb:${ err.message }`);
+  })
+
+mongoose.connection.on("disconnected", () => {
+  console.log(chalk.bold("Disconnected from mongodb"))
+})
+
+mongoose.connection.on("error", () => {
+  console.log(chalk.red(`Error with mongoDb ${err}`));
+})
+
+export default mongoose.connection
