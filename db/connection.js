7f959beff1ecd6ad3f923123fed604c7f5c56dfe
Diego Sousa <khesanh@Diegos-MacBook-Pro.local>
2023-07-24 15:26:58 -0400

    Deploying to heroku

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
 
+const connectionString = process.env.DB_URL || "mongodb://127.0.0.1:27017/my-api"
 mongoose.set("returnOriginal", false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/my-api").catch((err) => {
+
+mongoose.connect(connectionString).catch((err) => {
   console.log(`Can not connect to mongoDb:${err.message}`);
 });
 
